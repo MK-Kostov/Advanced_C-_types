a76b5a253cf35192bc320dce822e9dbb28410f86
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-26 08:44:52 -0500

    Reflection

--- Advanced_C#_types/Program.cs
+++ Advanced_C#_types/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+var converter = new ObjectToTextConverter();
+Console.WriteLine(converter.Convert(new House("123 Maple Road", 170.6, 2)));
+class ObjectToTextConverter
+{
+	public string Convert(object obj)
+	{
+		Type type = obj.GetType();
+		var properties = type.GetProperties().Where(p => p.Name != "EqualityContract");
+
+		return String.Join(", ", properties.Select(property => $"{property.Name} is {property.GetValue(obj)}"));
+	}
+}
+
+public record Pet(string Name, PetType PetType, float Weight);
+public record House(string Address, double Area, int Floors);
+public enum PetType { Cat, Dog, Fish }
